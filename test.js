0e3c0f613fa789102eaec3a7ad5ac2bbb9460c4a
Lin-Kyle <308088483@qq.com>
2018-06-27 18:12:19 +0800

    123

--- test.js
+++ test.js
@@ -1,43 +1,6 @@
-const EventEmitter = require('events');
-//創建類
-class MyEmitter extends EventEmitter {}
-//實例化
-const myEmitter = new MyEmitter();
-//監聽
-myEmitter.on('newListener', () => {
-    console.log('触发了一个事件！');
+const render = function(template, data) {
+    _.template(template, data);
+};
+$.get("template", template => {
+    $.get("data", data => {});
 });
-myEmitter.on('event', () => {
-  console.log('A');
-});
-//觸發
-myEmitter.emit('event');
-
-
-
-##EventProxy
-
-
-
-##Promise()
-
-
-
-##connect
-
-
-
-##async
-
-
-
-
-##step
-
-
-
-
-
-
-#異步并發控制
-##bagpipe
