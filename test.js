e98fd6dca4b7d7a201387b7c449dc0f93f83fbd8
linyk <linyk@yunyichina.cn>
2018-07-01 21:33:06 +0800

    1313

--- test.js
+++ test.js
@@ -1,11 +1,1 @@
-const EventEmitter = require('events');
-//創建類
-class MyEmitter extends EventEmitter {}
-//實例化
-const myEmitter = new MyEmitter();
-//監聽
-myEmitter.on('event', () => {
-    console.log('触发了一个事件！');
-});
-//觸發
-myEmitter.emit('event');
+console.log(module.paths);
